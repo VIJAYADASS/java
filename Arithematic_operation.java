fb07c9e65cb3119add037849c7fefbdf49994bf8
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-02-03 22:33:37 +0530

    Add files via upload

---
+++ Arithematic_operation.java
@@ -0,0 +1,27 @@
+package Basic;
+
+public class Arithematic_operation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 3;
+		int b = 2 - 1;
+		int c = 1*10;
+		int d = 10 / 20;
+		int e = 30%5;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		 		
+		
+		
+		
+		
+		
+	}
+
+}
