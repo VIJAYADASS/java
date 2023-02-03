a5d812a8a4db917bde88b48eb5fd63903706e748
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-02-03 22:01:15 +0530

    Create Data_types.java

---
+++ Data_types.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class Data_types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		short b = 5;
+		long c = 44;
+		double d = 10.f;
+		float e = 10;
+		boolean f = true;
+		char g = 'a';
+					
+	   System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+
+}
