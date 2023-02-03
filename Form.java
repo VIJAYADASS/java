4355acbbc73facbd4be284fb2e90e248b74de09e
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2023-02-03 23:35:18 +0530

    Add files via upload

---
+++ Form.java
@@ -0,0 +1,51 @@
+package Basic;
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+public class Form {
+	
+	
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s = new Scanner(System.in);
+	
+System.out.println("enter your name");
+String name = s.nextLine();
+System.out.println("enter your age");
+String age = s.nextLine();
+System.out.println("enter your dob");
+String dob= s.nextLine();
+System.out.println(" enter your email");
+String emaila= s.nextLine();
+System.out.println("enter your mobile number");
+String num= s.nextLine();
+System.out.println("enter your mark1");
+String mark1= s.nextLine();
+System.out.println("enter your mark2");
+String mark2= s.nextLine();
+System.out.println("enter your mark3");
+String mark3= s.nextLine();
+System.out.println("enter your avg");
+String avg  = s.nextLine();
+
+
+System.out.println("Name"+name);
+System.out.println("age" +age);
+System.out.println("dob"+dob);
+System.out.println("num"+ num);
+System.out.println("mark1"+ mark1);
+System.out.println("mark2"+mark2);
+System.out.println("mark3"+mark3);
+System.out.println("avg"+avg);
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
